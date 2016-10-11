6475d0ad2a13b54d5e515b8ed8a7f00235388136
Michael Gernoth <michael@gernoth.net>
2016-10-11 13:16:47 +0200

    beckham: Exclude Lineage Camera app
    * We use our moto Camera app.
    
    EDIT: This way allows us a little more flexability and
    doesn't require decommonizing the package for all devices.
    
    Change-Id: I5345fc03f45844bb10d629f100720e39fbbdf9ee

--- device.mk
+++ device.mk
@@ -45,6 +45,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+TARGET_SHIPS_MOTCAMERA := true
+
 # Dex-pre-opt exclusions
 $(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
 
@@ -80,6 +83,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # Sensors
 PRODUCT_COPY_FILES += \
