f54cc9556cd16fc9436765ff810e64b32a20955a
Anton Hansson <hansson@google.com>
2018-12-12 16:15:21 +0000

    beckham: Remove PRODUCT_AAPT config
    This config strips unused drawable resources from the device
    to save space, but it is not particularly effective in practice
    (roughly ~30 MB across all partitions).
    
    Remove it to make the system image more generic.
    
    Bug: 118676832
    Test: make
    Change-Id: I3ae120773e793af6e7b5759d383b7fa9c65aeb48

--- device.mk
+++ device.mk
@@ -23,11 +23,6 @@ $(call inherit-product, vendor/motorola/beckham/beckham-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# AAPT
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := 560dpi
-PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aov_ec_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/aov_ec_mixer_paths.xml \
