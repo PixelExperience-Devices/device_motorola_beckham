221b031578ad83850792ce433881c22e43ddeffd
Tri Vo <trong@google.com>
2019-02-01 11:27:25 -0800

    beckham: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
    We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
    extends system sepolicy. To that end, we use another make variable to
    specify product sepolicy.
    
    Bug: 119305624
    Test: m selinux_policy
    Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,8 +64,8 @@ TARGET_HAS_NO_WLAN_STATS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy-mods/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-mods/vendor
 
 # Inherit from the proprietary files
