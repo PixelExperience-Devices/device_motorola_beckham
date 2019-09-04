3fab18a392ace093f0cd955b3ff6bfe9ffa3392b
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:14:27 +0430

    beckham: inherit product api level makefile
    Change-Id: I970ef7f9a0d352a286395c7cad79dcaf3245f0ab
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,8 +43,5 @@ TARGET_HAS_NO_WLAN_STATS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := false
 
-# Treble
-PRODUCT_SHIPPING_API_LEVEL := 27
-
 # inherit from the proprietary version
 -include vendor/motorola/beckham/BoardConfigVendor.mk
