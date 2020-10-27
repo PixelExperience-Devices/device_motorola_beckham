d4f12f99f632929057394fcd928c0f9c5ea8c645
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-10-27 07:27:25 -0400

    beckham: Add aov_ec_mixer_paths for MotSpeakerHelper
    * Based on observing these in logs...
      - E audio_route: Failed to open /vendor/etc/aov_ec_mixer_paths.xml:
          No such file or directory
      - I MotSpeakerHelper: AOV_EC mixer_path xml not found
    
    Change-Id: Iba85d6b192ab408345785b28bbcd434ec72cae1c

--- device.mk
+++ device.mk
@@ -35,6 +35,7 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 PREBUILT_AUDIOHAL := true
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/aov_ec_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/aov_ec_mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
