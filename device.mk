b342ff4802fd27c52ae48c590a587a024c38103a
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-11-07 22:46:26 -0500

    beckham: Don't build audio hal
    * We're shipping prebuilt version already for Moto Mods support
      as per this commit: fcbf2bc1296ab394ab910d487f2044645ad3bd56
    
    Partially reverts: 72c1048c1cfc478302b9f3a64d1e83e0551c9aa4
    
    Change-Id: Iede887d44a98e5e64d30ea8aea69e7868a801f9d

--- device.mk
+++ device.mk
@@ -32,10 +32,6 @@ PRODUCT_AAPT_PREF_CONFIG := 560dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 # Audio
-PRODUCT_PACKAGES += \
-    audio.primary.sdm660 \
-    libqcompostprocbundle
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aov_ec_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/aov_ec_mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
